cf96edb9d3a06cdbf8e8bd1075b8649a0d3edc5a
ST10119495 <ST10119495@vcconnect.co.za>
2022-05-26 14:10:12 +0200

    View Item Page

---
+++ app/src/main/java/opscwork/viewitempagefeatures/ViewItem.java
@@ -0,0 +1,14 @@
+package opscwork.viewitempagefeatures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_item_page);
+    }
+}
