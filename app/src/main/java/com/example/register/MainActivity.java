e01cf2bb3ebb42fc110fa79d91de7db191a28681
kagiso <kagiso15masina@gmail.com>
2022-05-26 10:58:27 +0200

    Login and register page updated

--- app/src/main/java/com/example/register/MainActivity.java
+++ app/src/main/java/com/example/register/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.register;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,21 +26,31 @@ public class MainActivity extends AppCompatActivity {
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkDataEnter();
+                checkDataEntered();
 
-                String Username = username.getText().toString();
-                String Password = password.getText().toString();
-
-                if (username.equals("") || password.equals("")) {
-                    Toast.makeText(getApplicationContext(), "Fields Required", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "User name taken", Toast.LENGTH_SHORT).show();
-                }
             }
         });
-
     }
-
-     void checkDataEnter() {
+boolean isEmpty(EditText text){
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+}
+     void checkDataEntered() {
+        if(isEmpty(username)){
+            Toast t = Toast.makeText(this,"You must enter a username", Toast.LENGTH_SHORT);
+            t.show();
+        }
+        if(isEmpty(password)){
+            password.setError("Password is required");
+        }
     }
 }
+
+    //String Username = username.getText().toString();
+   // String Password = password.getText().toString();
+
+           //     if (username.equals("") || password.equals("")) {
+              //          Toast.makeText(getApplicationContext(), "Fields Required", Toast.LENGTH_SHORT).show();
+              //          } else {
+                 //       Toast.makeText(getApplicationContext(), "User name taken", Toast.LENGTH_SHORT).show();
+                 //       }
