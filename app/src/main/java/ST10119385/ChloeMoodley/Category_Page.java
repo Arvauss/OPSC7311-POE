f6d85d8041edc8e956521b762699cc29dc7eedb9
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-05-29 17:50:02 +0200

    Implemented Burger Menu into the category creation page.

--- app/src/main/java/ST10119385/ChloeMoodley/Category_Page.java
+++ app/src/main/java/ST10119385/ChloeMoodley/Category_Page.java
@@ -2,11 +2,15 @@ package ST10119385.ChloeMoodley;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.test.Dashboard_Activity;
 import com.example.test.R;
@@ -16,10 +20,27 @@ import com.example.test.R;
 
 public class Category_Page extends AppCompatActivity {
 
+    //Declarations for DrawerLayout (geeksforgeeks.org, 2022)
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category_ui_page);
+
+        // drawer layout instance to toggle the menu icon to open
+        //drawer and back button to close drawer (geeksforgeeks.org, 2022).
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button (geeksforgeeks.org, 2022).
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar (geeksforgeeks.org, 2022).
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         /*
             adapter made to push data (in the array string ) from the strings view to the
@@ -34,6 +55,21 @@ public class Category_Page extends AppCompatActivity {
         myAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); //making the adapter a dropdown
         dropDown.setAdapter(myAdapt);   //setting the spinner to the adapter
     }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked (geeksforgeeks.org, 2022).
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 //android on click = view
     public void GoBackDash (View v) {
         Intent goBackDash = new Intent (this, Dashboard_Activity.class);
