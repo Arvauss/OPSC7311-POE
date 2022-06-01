a2d5a21ee04901126d533d5155d85d1baaf58408
ChloeMoodley <ST10119385@vcconnect.edu.za>
2022-06-01 18:39:45 +0200

    UI UPDATES

---
+++ app/src/main/java/ST10119385/ChloeMoodley/item_list_all.java
@@ -0,0 +1,16 @@
+package ST10119385.ChloeMoodley;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.test.R;
+
+public class item_list_all extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_list_all);
+    }
+}
