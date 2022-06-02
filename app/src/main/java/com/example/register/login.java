025c26da36b76c4d4cc4b1b8b615097970ca07f9
Dewald-10119314 <st10119314@vcconnect.edu.za>
2022-06-02 22:40:19 +0200

    Reworked Login + Register classes to utilise ArrayList for temporary persistence

--- app/src/main/java/com/example/register/login.java
+++ app/src/main/java/com/example/register/login.java
@@ -3,25 +3,40 @@ package com.example.register;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.test.Dashboard_Activity;
 import com.example.test.R;
+
+import java.util.ArrayList;
 
 public class login extends AppCompatActivity  {
 
     EditText Username, Password;
     Button LoginButton;
 
+    public static ArrayList<LoginPageDataModel> userList = new ArrayList<LoginPageDataModel>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        if (userList.size() == 0){
+            LoginPageDataModel TestUser = new LoginPageDataModel("Testing.com", "Password123");
+            userList.add(TestUser);
+        }
+
         setupUI();
         setupListener();
 
@@ -37,56 +52,58 @@ public class login extends AppCompatActivity  {
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                verifyUsername();
-                Login(view);
+               if (verifyUsername()){
+                   Login(view);
+               } else {
+                   //prompt if user makes a wong input on password or username
+                   Toast t = Toast.makeText(getApplicationContext(), "Wrong username or Password", Toast.LENGTH_SHORT);
+                   t.show();
+               }
             }
         });
 
     }
 
     //This method is used to verify the username and password
-     void verifyUsername() {
+     public boolean verifyUsername() {
 
         //Method uses a boolean to check if the enter data is eiter true or false
-         boolean isValid = true;
+         boolean isInputValid = true;
 
          //If statement used to show an error message if the username text is empty
          if (isEmpty(Username)) {
              Username.setError("Enter username to login");
-             isValid = false;
+             isInputValid = false;
          }
          //If statement used to show an error message if the password text is empty
          if (isEmpty(Password)) {
              Password.setError("Enter password to login");
-             isValid = false;
+             isInputValid = false;
          } else {
              //Else if statement is used to check if the user password is longer than 4 characers
              if (Password.getText().toString().length() < 4) {
                  Password.setError("Password must be longer than 4 characters");
-                 isValid = false;
+                 isInputValid = false;
              }
          }
+
+         boolean isUserValid = false;
 
          //This if statement is used to validate the username and password
-         if (isValid) {
+         if (isInputValid) {
              String UsernameVal = Username.getText().toString();
              String PasswordVal = Password.getText().toString();
-
              //If statement evaluates if the password matches the username
-             if(UsernameVal.equals("Testing.com")&& PasswordVal.equals("Password123")){
-
-                 //Below intent is used to open new activity after login has been checked
-                 Intent i = new Intent(login.this, RegisterActivity.class);
-                 startActivity(i);
-                 //Finish the activity
-                 this.finish();
-
-                 //Else statement prompt if user makes a wong input on password or username
-             }else{
-                 Toast t = Toast.makeText(this,"Wrong username or Password", Toast.LENGTH_SHORT);
-                 t.show();
+             for (LoginPageDataModel user: userList) {
+                 if(UsernameVal.equals(user.getUsername()) && PasswordVal.equals(user.getPassword())){
+                     isUserValid = true;
+                     return isUserValid;
+                 } else{
+                     isUserValid = false;
+                 }
              }
          }
+         return isUserValid;
      }
 
      //boolean is used to evaluate any empty editTexts
