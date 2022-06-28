ad16eace0e6a8477e08a85d796b154fae445559d
Dewald-10119314 <st10119314@vcconnect.edu.za>
2022-06-28 21:38:27 +0200

    ViewAllItems list now functionally reads from DB

--- app/src/main/java/ST10119385/ChloeMoodley/item_list_all.java
+++ app/src/main/java/ST10119385/ChloeMoodley/item_list_all.java
@@ -17,11 +17,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.register.login;
 import com.example.test.Dashboard_Activity;
 import com.example.test.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 import opscwork.viewitempagefeatures.ItemList;
 import opscwork.viewitempagefeatures.ItemPage;
@@ -38,7 +47,12 @@ public class item_list_all extends AppCompatActivity {
     ImageView ItemImageAll;
     ListView ListViewAll;
 
+    ArrayList<Item_Information> ItemList = new ArrayList<>();
+    ArrayList<Category_Information> catList = new ArrayList<>();
+
     DatabaseReference dbRef;
+    FirebaseAuth Auth;
+    FirebaseUser user;
 
     //Declarations for DrawerLayout (geeksforgeeks.org, 2022)
     public DrawerLayout drawerLayout;
@@ -52,6 +66,8 @@ public class item_list_all extends AppCompatActivity {
         Log.d(TAG, "onCreate: Started.");
 
         dbRef = FirebaseDatabase.getInstance("https://bodegaapp-opscpoe-default-rtdb.firebaseio.com/").getReference();
+        Auth = FirebaseAuth.getInstance();
+        user = Auth.getCurrentUser();
 
         // drawer layout instance to toggle the menu icon to open
         //drawer and back button to close drawer (geeksforgeeks.org, 2022).
@@ -72,20 +88,14 @@ public class item_list_all extends AppCompatActivity {
         // to make the Navigation drawer icon always appear on the action bar (geeksforgeeks.org, 2022).
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ListViewAll = (ListView) findViewById(R.id.itemListAllView);
-        // Creation of adapter
-        ItemList adp = new ItemList(this, R.layout.item_list_template, ItemPage.ItemArrayList);
-        ListViewAll.setAdapter(adp);
 
-        ItemNameAll = (EditText) findViewById(R.id.ItemNameBox);
-        ItemDescriptionAll = (EditText) findViewById(R.id.ItemDescTextBox);
-        ItemPurchaseDateAll = (TextView) findViewById(R.id.DatePicker);
-        ItemPriceAll = (EditText) findViewById(R.id.priceTextBox);
-        ItemImageAll = (ImageView) findViewById(R.id.ImageItemPic);
 
-        setupUI();
-        setupOnClickListeners();
-        DisplayListData();
+        SetupUI();
+
+        InitListData();
+
+
+
 
     }
 
@@ -139,16 +149,74 @@ public class item_list_all extends AppCompatActivity {
         });
     }
 
-    private void setupUI() {
+    private void SetupUI() {
         ItemNameAll = (EditText) findViewById(R.id.ItemNameBox);
         ItemDescriptionAll = (EditText) findViewById(R.id.ItemDescTextBox);
         ItemPurchaseDateAll = (TextView) findViewById(R.id.DatePicker);
         ItemPriceAll = (EditText) findViewById(R.id.priceTextBox);
+        ItemImageAll = (ImageView) findViewById(R.id.ImageItemPic);
     }
 
-    private void DisplayListData() {
-        // Creation of item objects and adding them to ArrayList
+    private void InitListData() {
+        catList.clear();
+        DatabaseReference catRef = dbRef.child("categories");
 
+
+        //stores all user categories into arraylist
+        catRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                Category_Information catObj;
+                for(DataSnapshot ds : task.getResult().getChildren()){
+                    if (Objects.equals(ds.child("uid").getValue(String.class), user.getUid())){
+                        catObj = ds.getValue(Category_Information.class);
+                        //obj = new Category_Information(ds.getKey(), ds.child("category_Colour").getValue(Integer.class), ds.child("category_Name").getValue(String.class), ds.child("category_Description").getValue(String.class))
+                        catList.add(catObj);
+                    }
+                }
+                PopulateItemList();
+            }
+        });
+
+
+
+
+    }
+
+    private void PopulateItemList(){
+        // pulling data from DB into ArrayList for ListView
+        ItemList.clear();
+
+        DatabaseReference itmRef = dbRef.child("items");
+        //gets snapshot of all items
+        itmRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                Item_Information item;
+                //adds every category object in DB to catList
+                for(DataSnapshot ds : task.getResult().getChildren()){
+                    for(Category_Information cat: catList){
+                        if(ds.child("cat_ID").getValue(String.class).equals(cat.getCatID())){
+                            item = ds.getValue(Item_Information.class);
+                            ItemList.add(item);
+                        }
+                    }
+                }
+
+                //Setups listview and connects adapter (The IIE, 2022)
+                SetupListView();
+
+            }
+        });
+    }
+
+    private void SetupListView(){
+        ListViewAll = (ListView) findViewById(R.id.itemListAllView);
+        // Creation of adapter
+        ItemList adp = new ItemList(this, R.layout.item_list_template, ItemList);
+        ListViewAll.setAdapter(adp);
+
+        setupOnClickListeners();
     }
 
 
