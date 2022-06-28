b671535ab7a1a547a6a44d5994015ac45b75a2ce
Dewald-10119314 <st10119314@vcconnect.edu.za>
2022-06-28 22:44:31 +0200

    Made burgermenu visible on ItemPage

--- app/src/main/java/opscwork/viewitempagefeatures/ItemPage.java
+++ app/src/main/java/opscwork/viewitempagefeatures/ItemPage.java
@@ -3,6 +3,7 @@ package opscwork.viewitempagefeatures;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -16,12 +17,16 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.test.Dashboard_Activity;
 import com.example.test.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -31,9 +36,15 @@ import java.util.Objects;
 
 import ST10119385.ChloeMoodley.Add_Item_Page;
 import ST10119385.ChloeMoodley.Category_Information;
+import ST10119385.ChloeMoodley.Graph_Page;
 import ST10119385.ChloeMoodley.Item_Information;
+import ST10119385.ChloeMoodley.ShoppingList_Page;
 
 public class ItemPage extends  AppCompatActivity {
+    //Declarations for DrawerLayout (geeksforgeeks.org, 2022)
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    public NavigationView burgerNavigationView;
 
     // Declaration of variables (The IIE, 2022)
     private static final String TAG = "ItemPage";
@@ -65,6 +76,26 @@ public class ItemPage extends  AppCompatActivity {
         Log.d(TAG, "onCreate: Started.");
 
         dbRef = FirebaseDatabase.getInstance("https://bodegaapp-opscpoe-default-rtdb.firebaseio.com/").getReference();
+
+        // drawer layout instance to toggle the menu icon to open (The IIE, 2022)
+        //drawer and back button to close drawer (geeksforgeeks.org, 2022).
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        //Instantiating burgerNavigationView and binding it to view (Pulak, 2017).
+        burgerNavigationView = findViewById(R.id.burgerNavigationView);
+
+        //Setting navigation item listener (Pulak, 2017).
+        burgerNavigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button (geeksforgeeks.org, 2022).
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+
+        // to make the Navigation drawer icon always appear on the action bar (geeksforgeeks.org, 2022).
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         setupUI();
         //try statement to catch any potential errors (W3Schools, 2022)
@@ -184,5 +215,41 @@ public class ItemPage extends  AppCompatActivity {
             }
         });
 
+    }
+
+    // override the onOptionsItemSelected (The IIE, 2022)
+    // function to implement (The IIE, 2022)
+    // the item click listener callback (The IIE, 2022)
+    // to open and close the navigation (The IIE, 2022)
+    // drawer when the icon is clicked (geeksforgeeks.org, 2022).
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Method to handle the OnCLicked events within the burger menu (Pulak, 2017)
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here (The IIE, 2022)
+        int id = item.getItemId();
+
+        if (id == R.id.nav_dashboard) {
+            //Go to dashboard (The IIE, 2022)
+            Intent dashB = new Intent(this, Dashboard_Activity.class);
+            startActivity(dashB);
+        } else if (id == R.id.nav_shopping_list){
+            //Go to shopping list page (The IIE, 2022)
+            Intent shopList = new Intent(this, ShoppingList_Page.class);
+            startActivity(shopList);
+        } else if (id == R.id.nav_graph_screen) {
+            //Go to Graph page (The IIE, 2022)
+            Intent graphPage = new Intent(this, Graph_Page.class);
+            startActivity(graphPage);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
