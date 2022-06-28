817c55c6f2e4ec96693e4662361e036c7a779cc2
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-06-28 13:05:59 +0200

    Implemented burger menu into graph page

--- app/src/main/java/ST10119385/ChloeMoodley/Graph_Page.java
+++ app/src/main/java/ST10119385/ChloeMoodley/Graph_Page.java
@@ -1,13 +1,20 @@
 package ST10119385.ChloeMoodley;
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.test.Dashboard_Activity;
 import com.example.test.R;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -17,6 +24,7 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -28,10 +36,35 @@ public class Graph_Page extends AppCompatActivity {
     private String [] xData = {"Vegetables", "Spices", "Liquids", "Fruits", "Snacks"};
     PieChart PIE;
 
+    //Declarations for DrawerLayout (geeksforgeeks.org, 2022)
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    public NavigationView burgerNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.graph_page);
+
+        // drawer layout instance to toggle the menu icon to open
+        //drawer and back button to close drawer (geeksforgeeks.org, 2022).
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        //Instantiating burgerNavigationView and binding it to view (Pulak, 2017).
+        burgerNavigationView = findViewById(R.id.burgerNavigationView);
+
+        //Setting navigation item listener (Pulak, 2017).
+        burgerNavigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button (geeksforgeeks.org, 2022).
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar (geeksforgeeks.org, 2022).
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         Log.d(TAG, "onCreate: create chart");
 
         PIE = (PieChart) findViewById(R.id.PieChart);
@@ -114,6 +147,42 @@ public class Graph_Page extends AppCompatActivity {
 
 
 
+    }
+
+    //Method to handle the OnCLicked events within the burger menu (Pulak, 2017)
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_dashboard) {
+            //Go to dashboard
+            Intent dashB = new Intent(this, Dashboard_Activity.class);
+            startActivity(dashB);
+        } else if (id == R.id.nav_shopping_list){
+            //Go to shopping list page
+            Intent shopList = new Intent(this, ShoppingList_Page.class);
+            startActivity(shopList);
+        } else if (id == R.id.nav_graph_screen) {
+            //Go to Graph page
+            Intent graphPage = new Intent(this, Graph_Page.class);
+            startActivity(graphPage);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked (geeksforgeeks.org, 2022).
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
